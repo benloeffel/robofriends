3bf4c30e6a6a60f2fb4afe05f375e779f36f69fd
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-14 12:09:42 +0100

    Updates

---
+++ precache-manifest.e4032cdae128a7ceec0410b78232b1ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c747aaecd2f9b5f12d8d13abba57c2a8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1a9540d0c10c975b40f7",
+    "url": "/robofriends/static/js/2.f39d4415.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.f39d4415.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e327d9ef3bf5f9b78e9e",
+    "url": "/robofriends/static/js/main.fa9c64d5.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  }
+]);
