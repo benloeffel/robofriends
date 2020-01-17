f380f4126db258fc7e97254e243369b4f2c19b83
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-17 09:14:18 +0100

    Change a few header styles and add prop-types to the props passed down to the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
 import { Styled } from "theme-ui";
+import PropTypes from "prop-types";
 /** @jsx jsx */
 import { jsx } from "theme-ui";
+import SearchInput from "./SearchInput";
 
-const Header = () => {
+const Header = ({ searchTerms, searchChange, resultLength }) => {
   return (
-    <header sx={{ textAlign: "center" }}>
+    <header
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
       <Styled.h1>RoboFriends</Styled.h1>
       <Styled.p>Search and instantly find your Robot Friends!</Styled.p>
+      <SearchInput searchTerm={searchTerms} searchChange={searchChange} />
+      <Styled.p>{resultLength} Robots found.</Styled.p>
     </header>
   );
+};
+
+Header.propTypes = {
+  searchTerms: PropTypes.string.isRequired,
+  searchChange: PropTypes.func.isRequired,
+  resultLength: PropTypes.number.isRequired
 };
 
 export default Header;
