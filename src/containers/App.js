cad6f4957558239f1290da032ab7af870cd9cec5
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-21 13:59:41 +0100

    Fetch data from api instead of receiving the data from a static file

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "theme-ui";
 import theme from "../utils/theme";
-import { robots } from "../robots";
 import Header from "../components/Header";
 import CardList from "../components/CardList";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([searchTerm]);
+  const [searchResults, setSearchResults] = useState([]);
 
   const onSearchChange = e => {
     setSearchTerm(e.target.value);
   };
 
   useEffect(() => {
-    const results = robots.filter(robot =>
-      robot.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    async function getUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
 
-    setSearchResults(results);
+      const results = data.filter(robot => {
+        return robot.name.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+
+      setSearchResults(results);
+    }
+
+    getUsers();
   }, [searchTerm]);
 
   return (
