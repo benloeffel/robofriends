4728223a494fdc7fbb1f162160e43bc4514018b6
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-22 15:04:36 +0100

    Add action pending/success/loading actions to asyncronously fetch data from an external api

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,26 @@
-import { CHANGE_SEARCH_TERM } from "./constants";
+import {
+  CHANGE_SEARCH_TERM,
+  REQUEST_USERS_PENDING,
+  REQUEST_USERS_SUCCESS,
+  REQUEST_USERS_FAILED
+} from "./constants";
 
 export const setSearchTerm = text => ({
   type: CHANGE_SEARCH_TERM,
   payload: text
 });
+
+export const requestUsers = () => dispatch => {
+  dispatch({ type: REQUEST_USERS_PENDING });
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      dispatch({ type: REQUEST_USERS_SUCCESS, payload: data });
+    })
+    .catch(error => dispatch({ type: REQUEST_USERS_FAILED, payload: error }));
+};
 
 /**
  * For more information on Actions visit the link below:
