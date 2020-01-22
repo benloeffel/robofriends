dbfdf9c312614cfdc7843346f4f17365d06ec86e
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-22 15:09:35 +0100

    Add new redux-thunk & redux-logger to app and create a root reducer that combines all reducers

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { searchRobots } from "./reducers";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { searchUsers, requestUsers } from "./reducers";
 
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({ searchUsers, requestUsers });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
