f492379faec132c92e8e23237e2f7b2328a35a8c
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-22 15:07:39 +0100

    Create a new reducer that handles the actions for requesting users pending/success/failed

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,44 @@
-import { CHANGE_SEARCH_TERM } from "./constants";
+import {
+  CHANGE_SEARCH_TERM,
+  REQUEST_USERS_PENDING,
+  REQUEST_USERS_SUCCESS,
+  REQUEST_USERS_FAILED
+} from "./constants";
 
-const initialState = {
+const initialStateSearch = {
   searchTerm: ""
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchUsers = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_TERM:
       return Object.assign({}, state, {
         searchTerm: action.payload
+      });
+    default:
+      return state;
+  }
+};
+
+const initialStateUsers = {
+  isLoading: false,
+  users: [],
+  error: ""
+};
+
+export const requestUsers = (state = initialStateUsers, action = {}) => {
+  switch (action.type) {
+    case REQUEST_USERS_PENDING:
+      return Object.assign({}, state, { isPending: true });
+    case REQUEST_USERS_SUCCESS:
+      return Object.assign({}, state, {
+        users: action.payload,
+        isLoading: false
+      });
+    case REQUEST_USERS_FAILED:
+      return Object.assign({}, state, {
+        error: action.payload,
+        isLoading: false
       });
     default:
       return state;
