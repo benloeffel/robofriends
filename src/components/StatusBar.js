0b5b37ea5a3c451d27c0c317c36fd82c18abd7ac
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-26 14:00:10 +0100

    Clean up code and avoid repetitive paragraph tags & replace the loading emoji

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -6,35 +6,37 @@ import { Box } from "@theme-ui/components";
 const StatusBar = ({ loading, error, resultLength }) => {
   return (
     <Box sx={{ bg: "secondary", padding: "16px", textAlign: "center" }}>
-      {loading ? (
-        <Styled.p>
-          Please wait while we're trying to load your data.
-          <span role="img" aria-label="Unamused">
-            😒
-          </span>
-        </Styled.p>
-      ) : error ? (
-        <Styled.p>
-          Oh no! We've encountered an error!
-          <span role="img" aria-label="Screaming">
-            😱
-          </span>
-        </Styled.p>
-      ) : resultLength > 0 ? (
-        <Styled.p>
-          We have found {resultLength} robots matching your search.{" "}
-          <span role="img" aria-label="Robot">
-            🤖
-          </span>
-        </Styled.p>
-      ) : (
-        <Styled.p>
-          Sorry, we could not find any robots matching your search.
-          <span role="img" aria-label="Sad">
-            🙁
-          </span>
-        </Styled.p>
-      )}
+      <Styled.p>
+        {loading ? (
+          <>
+            Please wait while we're trying to load your data.
+            <span role="img" aria-label="Loading">
+              ⏳
+            </span>
+          </>
+        ) : error ? (
+          <>
+            Oh no! We've encountered an error!
+            <span role="img" aria-label="Screaming">
+              😱
+            </span>
+          </>
+        ) : resultLength > 0 ? (
+          <>
+            We have found {resultLength} robots matching your search.{" "}
+            <span role="img" aria-label="Robot">
+              🤖
+            </span>
+          </>
+        ) : (
+          <>
+            Sorry, we could not find any robots matching your search.
+            <span role="img" aria-label="Sad">
+              🙁
+            </span>
+          </>
+        )}
+      </Styled.p>
     </Box>
   );
 };
