90bd48d2544c6861ae1519f68b1a4c3ed4c2a894
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-30 12:40:10 +0100

    Add alt attributes to images to improve accessibility for screen-readers

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,10 @@ import { Card, Image } from "@theme-ui/components";
 const CardComponent = ({ id, username, name, email }) => {
   return (
     <Card sx={{ bg: "background", border: "2px solid black" }}>
-      <Image src={`https://robohash.org/${id}?size=200x200`} />
+      <Image
+        src={`https://robohash.org/${id}?size=200x200`}
+        alt={`Robot ${id}`}
+      />
       <Styled.h2 sx={{ color: "primary" }}>{name}</Styled.h2>
       <Styled.p sx={{ color: "text" }}>{username}</Styled.p>
       <Styled.p sx={{ color: "text" }}>{email}</Styled.p>
