a7bce8d818a19a4a8d0ac778e668a0e40c6df1cb
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-30 21:18:37 +0100

    Fix incorrectly referenced assets required for PWA

--- src/components/Head.js
+++ src/components/Head.js
@@ -10,27 +10,25 @@ const Head = () => {
         name="viewport"
         conent="width=device-width, initial-scale=1, shrink-to-fit=no"
       ></meta>
-
-      {/* PWA */}
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="apple-touch-icon.png"
       ></link>
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="favicon-16x16.png"
       />
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ec5b31" />
+      <link rel="manifest" href="manifest.json" />
+      <link rel="mask-icon" href="safari-pinned-tab.svg" color="#ec5b31" />
       <meta name="msapplication-TileColor" content="#ec5b31" />
       <meta name="theme-color" content="#ec5b31"></meta>
 
