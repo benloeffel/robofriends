320becf6869b780bdb4fe10ee08c89c3711ecce3
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-01-30 10:44:52 +0100

    Move the dark mode theme button to it's own component & stop the heading from re-rendering unless changes are actually taking palce

---
+++ src/components/ButtonDarkMode.js
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { useColorMode } from "theme-ui";
+import { Button } from "@theme-ui/components";
+
+const ButtonDarkMode = () => {
+  const [colorMode, setColorMode] = useColorMode();
+
+  return (
+    <Button
+      sx={{ position: "absolute", right: "16px" }}
+      onClick={e => {
+        setColorMode(colorMode === "default" ? "dark" : "default");
+      }}
+    >
+      Lights {colorMode === "default" ? "OFF" : "ON"}!
+    </Button>
+  );
+};
+
+export default ButtonDarkMode;
