0df41d7261175b9fe4c0233469b28011bc530849
Benjamin Löffel <benjaminloeffel@gmail.com>
2020-02-26 20:52:45 +0100

    Create reducers that takes care of changing the application state and dispatches the actions

---
+++ src/store/reducers.ts
@@ -0,0 +1,41 @@
+import { ApplicationState, ApplicationAction } from "./types";
+
+export const initialState: ApplicationState = {
+  searchTerm: "",
+  loading: false,
+  users: [],
+  error: {
+    name: "",
+    message: ""
+  }
+};
+
+const reducer = (state = initialState, action: ApplicationAction) => {
+  switch (action.type) {
+    case "CHANGE_SEARCH_TERM":
+      return Object.assign({}, state, {
+        searchTerm: action.payload
+      });
+    case "REQUEST_USERS_PENDING":
+      return Object.assign({}, state, { isLoading: true });
+    case "REQUEST_USERS_SUCCESS":
+      return Object.assign({}, state, {
+        users: action.payload,
+        isLoading: false
+      });
+    case "REQUEST_USERS_FAILED":
+      return Object.assign({}, state, {
+        error: action.payload,
+        isLoading: false
+      });
+    default:
+      return state;
+  }
+};
+
+export default reducer;
+
+// /**
+//  * For more information on Reducers visit the link below:
+//  * https://redux.js.org/basics/reducers
+//  */
